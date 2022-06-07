7dd52af4906fbf8e04f67a06b4685dfef663e016
Logan Miller <loganmiller214@gmail.com>
2022-06-07 14:14:50 -0400

    params lab

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+  wrap_parameters format: []
+  
   # GET /birds
   def index
     birds = Bird.all
@@ -8,18 +10,26 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    # GET /birds/:id
+    def show
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
     end
+
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
+
 
 end
